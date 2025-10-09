305b2a5d03b609268b2fab0c14bf08578fb4f7ec
donkoo24 <donkoo@live.com.sg>
2025-10-09 15:59:24 +0800

    Create shift class

--- src/main/java/seedu/address/model/person/staff/Shift.java
+++ src/main/java/seedu/address/model/person/staff/Shift.java
@@ -1,4 +1,24 @@
-package seedu.address.model.person.customer;
+package seedu.address.model.person.staff;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Shift {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public Shift(LocalDateTime startTime, LocalDateTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Duration getDuration() {
+        return Duration.between(startTime, endTime);
+    }
+
+    @Override
+    public String toString() {
+        return "From: " + startTime.toString() + "\n" + "To: " + endTime.toString();
+    }
 }
+
