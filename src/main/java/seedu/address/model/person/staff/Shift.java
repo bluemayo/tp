595be39e61d65546cd280a495cf7a25b7941905c
donkoo24 <donkoo@live.com.sg>
2025-10-09 16:09:49 +0800

    Update javadocs and staff class with shifts

--- src/main/java/seedu/address/model/person/staff/Shift.java
+++ src/main/java/seedu/address/model/person/staff/Shift.java
@@ -1,17 +1,30 @@
 package seedu.address.model.person.staff;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+/**
+ * Represents a staff's shift in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Shift {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
+    /**
+     * Constructor that creates Shift class with startTime and endTime.
+     */
     public Shift(LocalDateTime startTime, LocalDateTime endTime) {
+        requireAllNonNull(startTime, endTime);
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
+    /**
+     * Returns an immutable Duration between startTime and endTime.
+     */
     public Duration getDuration() {
         return Duration.between(startTime, endTime);
     }
--- src/main/java/seedu/address/model/person/staff/Staff.java
+++ src/main/java/seedu/address/model/person/staff/Staff.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person.staff;
 
+import java.util.List;
 import java.util.Set;
 
 import seedu.address.model.person.Address;
@@ -9,11 +10,19 @@ import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
+/**
+ * Represents a staff in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Staff extends Person {
+    private List<Shift> shifts;
+
     /**
      * Constructor that calls super class constructor and initialise other relevant fields
      */
     public Staff(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags);
     }
+
+
 }
