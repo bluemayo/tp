b1af48963b5fa730af24e303deedb2ba0d580041
Gabriel Yap <gabrielyap37@gmail.com>
2025-10-11 16:41:48 +0800

    Support parser for add customer command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -1,20 +1,23 @@
     package seedu.address.logic.parser;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
+
+    import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+    import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+    import java.util.logging.Logger;
+    import java.util.regex.Matcher;
+    import java.util.regex.Pattern;
+
+    import seedu.address.commons.core.LogsCenter;
+    import seedu.address.logic.commands.AddCustomerCommand;
+    import seedu.address.logic.commands.ClearCommand;
+    import seedu.address.logic.commands.Command;
+    import seedu.address.logic.commands.DeleteCommand;
+    import seedu.address.logic.commands.EditCommand;
+    import seedu.address.logic.commands.ExitCommand;
+    import seedu.address.logic.commands.FindCommand;
+    import seedu.address.logic.commands.HelpCommand;
+    import seedu.address.logic.commands.ListCommand;
+    import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses user input.
  */
@@ -43,8 +46,8 @@ public class AddressBookParser {
         // Lower level log messages are used sparingly to minimize noise in the code.
         logger.fine("Command word: " + commandWord + "; Arguments: " + arguments);
         switch (commandWord) {
-        case AddCommand.COMMAND_WORD:
-            return new AddCommandParser().parse(arguments);
+        case AddCustomerCommand.COMMAND_WORD:
+            return new AddCustomerCommandParser().parse(arguments);
         case EditCommand.COMMAND_WORD:
             return new EditCommandParser().parse(arguments);
         case DeleteCommand.COMMAND_WORD:
