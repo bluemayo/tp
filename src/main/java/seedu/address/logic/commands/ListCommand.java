1e4abf8dc79b7081e1001f12cfcf80ae5fc87538
biscake <leonut.ljh@gmail.com>
2025-10-11 11:50:03 +0800

    Update Category enum and implemented logic for ListCommand

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,25 +1,52 @@
 package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Lists all persons in the address book to the user.
  */
 public class ListCommand extends Command {
+    public enum Category {
+        ALL("all", Model.PREDICATE_SHOW_ALL_PERSONS),
+        STAFF("staff", Model.PREDICATE_SHOW_ALL_STAFFS),
+        CUSTOMER("customer", Model.PREDICATE_SHOW_ALL_CUSTOMERS),
+        SUPPLIER("supplier", Model.PREDICATE_SHOW_ALL_SUPPLIERS);
 
-    public static enum Category {
-        ALL,
-        STAFF,
-        CUSTOMER,
-        SUPPLIER
+        private final String keyword;
+        private final Predicate<Person> predicate;
+
+        Category(String keyword, Predicate<Person> predicate) {
+            this.keyword = keyword;
+            this.predicate = predicate;
+        }
+
+        private Predicate<Person> getPredicate() {
+            return predicate;
+        }
+
+        public static Category fromString(String input) {
+            String trimmedToLowerCaseInput = input.trim().toLowerCase();
+            for (Category category : values()) {
+                if (category.keyword.equals(trimmedToLowerCaseInput)) {
+                    return category;
+                }
+            }
+
+            return null;
+        }
     }
 
     public static final String COMMAND_WORD = "list";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": List <all | staff | customer | supplier> categories";
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
-    private static final String MESSAGE_SUCCESS_FORMAT = "Listed %s";
+    public static final String MESSAGE_SUCCESS_FORMAT = "Listed %s";
 
-    private Category category;
+    private final Category category;
 
     public ListCommand(Category category) {
         this.category = category;
@@ -27,7 +54,8 @@ public class ListCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) {
-        // TODO: Implement logic
+        requireNonNull(model);
+        model.updateFilteredPersonList(category.getPredicate());
         return new CommandResult(String.format(MESSAGE_SUCCESS_FORMAT, this.category.toString().toLowerCase()));
     }
 }
