5e5b251b57ee3bb21b2696da5a3bdd7ccc1be41e
donkoo24 <donkoo@live.com.sg>
2025-10-15 13:11:15 +0800

    Fix style

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -47,8 +47,8 @@ import seedu.address.model.tag.Tag;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddCommandParserTest {
+    private static final String CUSTOMER_COMMAND = Person.ContactType.CUSTOMER.lowerCase() + " ";
     private AddCommandParser parser = new AddCommandParser();
-    private final static String CUSTOMER_COMMAND = Person.ContactType.CUSTOMER.lowerCase() + " ";
 
     // Test cases for Customer
     @Test
@@ -141,7 +141,9 @@ public class AddCommandParserTest {
     public void parse_optionalFieldsMissing_success() {
         // zero tags
         Customer expectedCustomer = new PersonBuilder(AMY).withTags().buildCustomer();
-        assertParseSuccess(parser, CUSTOMER_COMMAND + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY + ADDRESS_DESC_AMY,
+        assertParseSuccess(parser, CUSTOMER_COMMAND + NAME_DESC_AMY
+                        + PHONE_DESC_AMY + EMAIL_DESC_AMY
+                        + ADDRESS_DESC_AMY,
                 new AddCustomerCommand(expectedCustomer));
     }
 
@@ -150,50 +152,72 @@ public class AddCommandParserTest {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCustomerCommand.MESSAGE_USAGE);
 
         // missing name prefix
-        assertParseFailure(parser, CUSTOMER_COMMAND + VALID_NAME_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
+        assertParseFailure(parser, CUSTOMER_COMMAND + VALID_NAME_BOB
+                        + PHONE_DESC_BOB + EMAIL_DESC_BOB
+                        + ADDRESS_DESC_BOB,
                 expectedMessage);
 
         // missing phone prefix
-        assertParseFailure(parser, CUSTOMER_COMMAND + NAME_DESC_BOB + VALID_PHONE_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                        + NAME_DESC_BOB + VALID_PHONE_BOB
+                        + EMAIL_DESC_BOB + ADDRESS_DESC_BOB,
                 expectedMessage);
 
         // missing email prefix
-        assertParseFailure(parser, CUSTOMER_COMMAND + NAME_DESC_BOB + PHONE_DESC_BOB + VALID_EMAIL_BOB + ADDRESS_DESC_BOB,
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                        + NAME_DESC_BOB + PHONE_DESC_BOB
+                        + VALID_EMAIL_BOB + ADDRESS_DESC_BOB,
                 expectedMessage);
 
         // missing address prefix
-        assertParseFailure(parser, CUSTOMER_COMMAND + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + VALID_ADDRESS_BOB,
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                        + NAME_DESC_BOB + PHONE_DESC_BOB
+                        + EMAIL_DESC_BOB + VALID_ADDRESS_BOB,
                 expectedMessage);
 
         // all prefixes missing
-        assertParseFailure(parser, CUSTOMER_COMMAND + VALID_NAME_BOB + VALID_PHONE_BOB + VALID_EMAIL_BOB + VALID_ADDRESS_BOB,
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                        + VALID_NAME_BOB + VALID_PHONE_BOB
+                        + VALID_EMAIL_BOB + VALID_ADDRESS_BOB,
                 expectedMessage);
     }
 
     @Test
     public void parse_invalidValue_failure() {
         // invalid name
-        assertParseFailure(parser, CUSTOMER_COMMAND + INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                + INVALID_NAME_DESC + PHONE_DESC_BOB
+                + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
                 + TAG_DESC_AMY + TAG_DESC_AMY_2, Name.MESSAGE_CONSTRAINTS);
 
         // invalid phone
-        assertParseFailure(parser, CUSTOMER_COMMAND + NAME_DESC_BOB + INVALID_PHONE_DESC + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                + NAME_DESC_BOB + INVALID_PHONE_DESC
+                + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
                 + TAG_DESC_AMY + TAG_DESC_AMY_2, Phone.MESSAGE_CONSTRAINTS);
 
         // invalid email
-        assertParseFailure(parser, CUSTOMER_COMMAND + NAME_DESC_BOB + PHONE_DESC_BOB + INVALID_EMAIL_DESC + ADDRESS_DESC_BOB
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                + NAME_DESC_BOB + PHONE_DESC_BOB
+                + INVALID_EMAIL_DESC + ADDRESS_DESC_BOB
                 + TAG_DESC_AMY + TAG_DESC_AMY_2, Email.MESSAGE_CONSTRAINTS);
 
         // invalid address
-        assertParseFailure(parser, CUSTOMER_COMMAND + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                + NAME_DESC_BOB + PHONE_DESC_BOB
+                + EMAIL_DESC_BOB + INVALID_ADDRESS_DESC
                 + TAG_DESC_AMY + TAG_DESC_AMY_2, Address.MESSAGE_CONSTRAINTS);
 
         // invalid tag
-        assertParseFailure(parser, CUSTOMER_COMMAND + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                + NAME_DESC_BOB + PHONE_DESC_BOB
+                + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
                 + INVALID_TAG_DESC + VALID_TAG_BOB, Tag.MESSAGE_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
-        assertParseFailure(parser, CUSTOMER_COMMAND + INVALID_NAME_DESC + PHONE_DESC_BOB + EMAIL_DESC_BOB
+        assertParseFailure(parser, CUSTOMER_COMMAND
+                + INVALID_NAME_DESC
+                + PHONE_DESC_BOB + EMAIL_DESC_BOB
                 + INVALID_ADDRESS_DESC, Name.MESSAGE_CONSTRAINTS);
     }
 }
